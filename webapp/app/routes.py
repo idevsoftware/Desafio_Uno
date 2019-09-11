50d9287a85e472e499cb3f178b11a08c586fea49
Carlos Oliva (MBP) <carlos@idev.mobi>
2019-09-10 23:41:19 -0300

    Solucion al Desafio 1 (Periodos Perdidos).
     * Autor: Carlos Oliva G. <carlos@idev.mobi>
     * Enterado via Twitter (@lnds) [https://twitter.com/lnds/status/1169204551179788292]

---
+++ webapp/app/routes.py
@@ -0,0 +1,36 @@
+import urllib.request, json
+from urllib.parse import urlparse
+from app import app
+
+def to_json(data):
+    response = app.response_class(response=json.dumps(data), mimetype='application/json')
+    return response
+    
+@app.route('/')
+def index():
+    url = app.config['DATA_URL']
+    with urllib.request.urlopen(url) as data_url:
+        data = json.load(data_url)
+        start_date = data['fechaCreacion']
+        end_date = data['fechaFin']
+        existing_dates = set(data['fechas'])
+        start_year, start_month, *_ = list(map(int, start_date.split('-')))
+        end_year, end_month, *_ = list(map(int, end_date.split('-')))
+        missing_dates = []
+        for y in range(start_year, end_year + 1):
+            for m in range(1, 13):
+                if y == end_year and m > end_month:
+                    break
+                if y == start_year and m < start_month:
+                    continue
+                some_date = '{}-{:02d}-{:02d}'.format(y, m, 1)
+                if some_date not in existing_dates:
+                    missing_dates.append(some_date)
+        del data['fechas']
+        data['fechasFaltantes'] = missing_dates
+        return to_json(data)
+
+@app.route('/sampledata')
+def sampledata():
+    data = {'id': 6, 'fechaCreacion': '1969-03-01', 'fechaFin': '1970-01-01', 'fechas': ['1969-03-01', '1969-05-01', '1969-09-01', '1970-01-01']}
+    return to_json(data)
---
+++ webapp/config.py
@@ -0,0 +1,4 @@
+import os
+
+class Config(object):
+    DATA_URL = 'http://localhost:5000/sampledata'
